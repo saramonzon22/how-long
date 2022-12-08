0de6a819234c15a5d6dc41b64d3720bee20402f7
Sara Monzón Quesada <sara.mon.quesada@gmail.com>
2022-12-08 13:45:49 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,3 @@
-## En este segundo ejercicios tendréis que crear un script que nos diga si es la hora de ir a casa. Tendréis que hacer uso del modulo time. Necesitaréis la fecha del sistema y poder comprobar la hora.
-
-## En el caso de que sean más de las 7, se mostrará un mensaje y en caso contrario, haréis una operación para calcular el tiempo que queda de trabajo.
-
-
-
 import time
 hora = time.strftime('%H')
 minutos = time.strftime('%M')
